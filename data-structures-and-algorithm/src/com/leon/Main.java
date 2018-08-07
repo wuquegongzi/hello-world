01bffbf84254a80e03fd1f954864080aa9d592a4
Leon <swchenminglei@163.com>
2018-08-07 17:23:34 +0800

    Add files via upload

---
+++ data-structures-and-algorithm/src/com/leon/Main.java
@@ -0,0 +1,23 @@
+package com.leon;
+
+import com.leon.array.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("欢迎来到leon的数据结构世界~ \n " +
+                "Array 自我实现的数组 \n");
+
+        Array<Integer> arr= new Array<Integer>();
+        for (int i = 0; i < 10 ; i++){
+            arr.addLast(i);
+        }
+        System.out.println(arr);
+        arr.add(0,19);
+        System.out.println(arr.toString());
+        arr.addFirst(-1);
+        System.out.println(arr);
+    }
+
+}
