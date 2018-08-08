07db9157f058554dd36a3a2e05c1f85ef856cfb8
swleon <swchenminglei@163.com>
2018-08-08 16:53:09 +0800

    数组队列

---
+++ data-structures-and-algorithm/src/com/leon/strucyures/ArrayQueue.java
@@ -0,0 +1,94 @@
+package com.leon.strucyures;
+
+/**
+ * @package: com.leon.strucyures
+ * @author: 陈明磊<minglei.chen @ gm-medicare.com>
+ * @date: 2018/8/8 16:37
+ * @ModificarionHistory who     when   what
+ * --------------|------------------|--------------
+ */
+public class ArrayQueue<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public ArrayQueue(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue(){
+        array = new Array();
+    }
+
+    /**
+     * 入队
+     * @param e
+     */
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    /**
+     * 出队
+     * @return
+     */
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    /**
+     * 获取队首
+     * @return
+     */
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append("Queue :");
+        res.append("front [");
+        for (int i = 0 ; i < array.getSize() ; i++){
+            res.append(array.get(i));
+
+            if(i != array.getSize() - 1){
+                res.append(",");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+
+        for (int i=0 ; i < 10 ; i++){
+            queue.enqueue(i);
+            System.out.println(queue);
+            if( i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+
+    }
+
+}
---
+++ data-structures-and-algorithm/src/com/leon/strucyures/Queue.java
@@ -0,0 +1,42 @@
+package com.leon.strucyures;
+
+/**
+ * 队列接口
+ * @package: com.leon.strucyures
+ * @author: 陈明磊<swchenminglei@163.com>
+ * @date: 2018/8/8 16:33
+ * @ModificarionHistory who     when   what
+ * --------------|------------------|--------------
+ */
+public interface Queue<E> {
+
+    /**
+     * 入队
+     * @param e
+     */
+    void enqueue(E e);
+
+    /**
+     * 出队
+     * @return
+     */
+    E dequeue();
+
+    /**
+     * 获取队首
+     * @return
+     */
+    E getFront();
+
+    /**
+     * 获取队列大小
+     * @return
+     */
+    int getSize();
+
+    /**
+     * 判断队列是否为空
+     * @return
+     */
+    boolean isEmpty();
+}
