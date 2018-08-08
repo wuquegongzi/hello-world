a78d93a0f52a11f06aac8df182e165263171fe31
swleon <swchenminglei@163.com>
2018-08-08 16:24:40 +0800

    栈的应用：       括号匹配-编译器  ValidParentheses  ValidParentheses02

---
+++ data-structures-and-algorithm/readme.md
@@ -0,0 +1,12 @@
+数据结构与算法
+
+com.leon.strucyures  数据结构实现模拟
+   Array 动态数组实现
+   ArrayStack 数组栈实现
+   
+
+com.leon.leetcode    leetcode算法实现
+   栈的应用：
+       1、undo操作-编辑器  
+       2、系统调用栈-操作系统  
+       3、括号匹配-编译器  ValidParentheses  ValidParentheses02
---
+++ data-structures-and-algorithm/src/com/leon/leetcode/ValidParentheses02.java
@@ -0,0 +1,63 @@
+package com.leon.leetcode;
+
+
+import com.leon.strucyures.ArrayStack;
+
+/**
+ * Solution20 https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * @package: com.leon.leetcode
+ * @author: 陈明磊<swchenminglei@163.com>
+ * @date: 2018/8/8 16:00
+ * @ModificarionHistory who     when   what
+ * --------------|------------------|--------------
+ */
+public class ValidParentheses02 {
+
+   public boolean isValid(String s){
+
+       ArrayStack<Character> stack = new ArrayStack<Character>();
+       for(int i = 0 ; i < s.length() ; i++){
+           char c = s.charAt(i);
+           if(c == '{' || c == '[' || c == '('){
+              stack.push(c);
+           }else{
+               if(stack.isEmpty()){
+                   return false;
+               }
+
+               char topChar = (char)stack.pop();
+               if(c == ')' && topChar != '('){
+                   return false;
+               }
+               if(c == '}' && topChar != '{'){
+                   return false;
+               }
+               if(c == ']' && topChar != '['){
+                   return false;
+               }
+           }
+       }
+
+       return stack.isEmpty();
+   }
+
+    public static void main(String[] args) {
+
+        String s = "[{(]}]";
+
+        ValidParentheses02 validParentheses = new ValidParentheses02();
+
+        System.out.println(validParentheses.isValid(s));
+    }
+
+
+}
