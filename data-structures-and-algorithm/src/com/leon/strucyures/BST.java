6b3cfef4ec4a97c6e7800797201407288e928831
swleon <swchenminglei@163.com>
2018-09-17 20:09:33 +0800

    二分搜索树 删除任意节点

--- data-structures-and-algorithm/src/com/leon/strucyures/BST.java
+++ data-structures-and-algorithm/src/com/leon/strucyures/BST.java
@@ -311,6 +311,57 @@ public class BST<E extends Comparable<E>>{
     }
 
     /**
+     * 删除元素e
+     * @param e
+     */
+    public void remove(E e){
+
+        root = remove(root,e);
+    }
+
+    private Node remove(Node node, E e) {
+
+        if(node == null){
+            return null;
+        }
+
+        if(e.compareTo(node.e) < 0){
+            node.left = remove(node.left,e);
+            return node;
+        }else if(e.compareTo(node.e) > 0){
+            node.right = remove(node.right,e);
+            return node;
+        }else{ //e == node.e
+
+            //待删除节点左子树为空的情况
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            //待删除节点右子树为空的情况
+            if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            //待删除节点左右子树均不为空的情况
+            //找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
+    /**
      *
      * @return
      */
