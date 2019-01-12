830afda634ad4e1da20196bd4cd390d1bed8079a
leon <swchenminglei@163.com>
2019-01-12 15:34:25 +0800

    高并发

---
+++ high_concurrent/src/main/java/com/leon/highconcurrent/HighConcurrentApplication.java
@@ -0,0 +1,14 @@
+package com.leon.highconcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighConcurrentApplication.class, args);
+    }
+
+}
+
