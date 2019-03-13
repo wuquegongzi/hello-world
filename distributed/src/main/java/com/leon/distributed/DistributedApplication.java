66b45988ee87868e0f3bec63bedb4614a26ce594
swleon <swchenminglei@163.com>
2019-03-13 18:03:45 +0800

    分布式全局唯一ID生成策略

---
+++ distributed/src/main/java/com/leon/distributed/DistributedApplication.java
@@ -0,0 +1,13 @@
+package com.leon.distributed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedApplication.class, args);
+    }
+
+}
